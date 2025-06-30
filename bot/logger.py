d2c437b175136b05414ebac36a5f1a0209d41700
dg <dg@nrc.ac.uk>
2025-06-30 23:50:21 +0100

    Switch logger to stdout for Railway compatibility

--- bot/logger.py
+++ bot/logger.py
@@ -1,13 +1,15 @@
 import logging
+import sys
 
-def setup_logger(name="intelcrux", logfile="bot.log"):
+def setup_logger(name="intelcrux"):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
     if not logger.handlers:
-        fh = logging.FileHandler(logfile)
+        # Stream logs to stdout (Railway Logs tab)
+        handler = logging.StreamHandler(sys.stdout)
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
     return logger
